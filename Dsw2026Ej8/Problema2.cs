dbf73ed4bcf6a6e40c6cd4964ad9de4bd0de9de0
Samir Morhill <Samir.Morhill@alu.frt.utn.edu.ar>
2026-05-03 12:28:58 -0300

    Problema N°2 resuelto.

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal totalCalculado = quantity <= 0 ? 0 : quantity * unitPrice;
+
+            var resumenVenta = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = totalCalculado,
+            };
+
+            return $"{resumenVenta.Code}-{resumenVenta.Description}-{resumenVenta.Total}";
+        }
+
+    }
+}
