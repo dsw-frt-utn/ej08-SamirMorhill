70ce40377481c43f7a43961fa7ffefb99117fc66
Samir Morhill <Samir.Morhill@alu.frt.utn.edu.ar>
2026-05-03 13:03:14 -0300

    Problema N°4 resuelto.

---
+++ Dsw2026Ej8/Problem4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problem4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            double suma = 0;
+            int cantidadNotas = 0;
+
+            if(nota1.HasValue && nota1.Value < 0 && nota1.Value > 10)
+            {
+                suma += nota1.Value;
+                cantidadNotas++;
+            }
+
+            if (nota2.HasValue && nota2.Value < 0 && nota2.Value > 10)
+            {
+                suma += nota2.Value;
+                cantidadNotas++;
+            }
+
+            if (nota3.HasValue && nota3.Value < 0 && nota3.Value > 10)
+            {
+                suma += nota3.Value;
+                cantidadNotas++;
+            }
+
+            if (cantidadNotas == 0)
+            {
+                return 0;
+            }
+
+            return suma / cantidadNotas;
+
+        }
+    }
+}
