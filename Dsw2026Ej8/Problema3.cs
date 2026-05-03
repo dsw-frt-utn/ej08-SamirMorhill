92c61bc5a419dbed0b0f9d28ec7f0ec5b724c16e
Samir Morhill <Samir.Morhill@alu.frt.utn.edu.ar>
2026-05-03 12:44:25 -0300

    Problema N°3 resuelto.

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int copyValue = originalValue;
+
+            copyValue++;
+
+            Product copiedProduct = product;
+
+            copiedProduct.ActualDescription("New description");
+
+            return $"{originalValue}-{copyValue}-{copiedProduct.Description}";
+
+
+
+        }
+
+
+
+
+    }
+}
